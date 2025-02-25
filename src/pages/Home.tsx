eae9093c9885f3190e9580bbab5a1c1be26109d7
Eliott BIDAULT-HERVOUET <github.wavy095@passmail.net>
2025-02-25 22:16:03 +0100

    Added home page and commands choice

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,22 @@
+import { Link } from 'react-router';
 
 const Home = () => {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello backlog!
-        </h1>
-    );
+	return (
+		<div className="flex flex-row flex-wrap justify-center items-center w-screen h-screen p-15 gap-15">
+			<Link to="/produits" className="flex flex-row justify-center items-center size-2/5 bg-amber-700 rounded-lg">
+				Produits
+			</Link>
+			<Link to="/choix-commandes" className="flex flex-row justify-center items-center size-2/5 bg-blue-700 rounded-lg">
+				Commandes
+			</Link>
+			<Link to="/employes" className="flex flex-row justify-center items-center size-2/5 bg-red-600 rounded-lg">
+				Employés
+			</Link>
+			<Link to="/fournisseurs" className="flex flex-row justify-center items-center size-2/5 bg-lime-500 rounded-lg">
+				Fournisseurs
+			</Link>
+		</div>
+	);
 };
 
 export default Home;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import ChoixCommandes from '../pages/ChoixCommandes';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/choix-commandes' element={<ChoixCommandes/>}/>
             </Routes>
         </BrowserRouter>
     );
