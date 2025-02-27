9b9adf319bd40b5aae90353aae151954fdef2f84
Eliott BIDAULT-HERVOUET <github.wavy095@passmail.net>
2025-02-28 00:41:17 +0100

    Added the modify of the employee

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Employes from '../pages/Employes';
+import EmployeeDetails from '../pages/EmployeeDetails';
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/employes' element={<Employes/>}/>
+                <Route path="/employes/:id" element={<EmployeeDetails />} />
             </Routes>
         </BrowserRouter>
     );
